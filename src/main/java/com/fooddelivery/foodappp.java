023fea6cf24ac03c2a6bb4b04a97734d5ee7ddee
dipankarsk <dipankarsrkr23@gmail.com>
2021-11-16 21:30:23 +0530

    testing 1

---
+++ src/main/java/com/fooddelivery/foodappp.java
@@ -0,0 +1,13 @@
+package com.fooddelivery;
+
+/**
+ * Hello world!
+ */
+public final class foodappp {
+   
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
