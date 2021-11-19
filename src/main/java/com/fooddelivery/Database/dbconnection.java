83f5a137293f4ed1267a062bd9b1367acf01a652
Shivam20304 <shivam20304@iiitd.ac.in>
2021-11-19 15:06:46 +0530

    Registration process

--- src/main/java/com/fooddelivery/Database/dbconnection.java
+++ src/main/java/com/fooddelivery/Database/dbconnection.java
@@ -3,27 +3,47 @@ package com.fooddelivery.Database;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import com.fooddelivery.Authentication.registration;
+
 public class dbconnection {
+    public static void insertUserData ()
+    {
+        Connection con =  Dbconnection("test.db");
+        registration reg = new registration();
+        String sql = "INSERT INTO User(UserName,EmailId , Password) VALUES(?,?,?)";
 
-    public static void createNewDatabase(String fileName) {
+        try {
+            PreparedStatement usri = con.prepareStatement(sql);
+                usri.setString(1, reg.getUserName());
+                usri.setString(2, reg.getEmailId());
+                usri.setString(2, reg.getPassword());
+                usri.executeUpdate();
+        }
+            
+        catch (SQLException e) {
+            System.out.println("Error due to insertion " + e.getMessage());
+        }
+    }
 
-        String url = "jdbc:sqlite:./" + fileName;
+    public static Connection Dbconnection(String name_file) {
 
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
+        String u = "jdbc:sqlite:./" + name_file;
+
+        try (Connection connect = DriverManager.getConnection(u)) {
+            if (connect != null) {
                 
-                System.out.println("A new database has been created.");
+                return connect;
             }
 
         } catch (SQLException e) {
             System.out.println("Error"+e.getMessage());
         }
+        return null ;
     }
 
-    public static void main(String[] args) {
-        createNewDatabase("test.db");
-    }
+    
     
 }
--- src/main/java/com/fooddelivery/foodappp.java
+++ src/main/java/com/fooddelivery/foodappp.java
@@ -1,10 +1,59 @@
 package com.fooddelivery;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.fooddelivery.Authentication.registration;
+import com.fooddelivery.Database.dbconnection;
+
 public final class foodappp {
    
 
+    public static void register ()
+    {
+        dbconnection.insertUserData();
+
+    }
     
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println("Welcom to XYZ food delivery system");
+        while(true)
+        {
+            System.out.println("Do you want to 1)Login 2)Register");
+            InputStreamReader ir =  new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(ir);
+            int choice = Integer.parseInt(br.readLine());
+            switch (choice)
+            {
+            case 1 :
+            System.out.println("Username : ");
+            String userName = br.readLine();
+            System.out.println("Password : ");
+            String password = br.readLine();
+
+            case 2 :
+            registration reg = new registration();
+            System.out.println("Username : ");
+            String f_user = br.readLine();
+            System.out.println("EmailId : ");
+            String f_emailId = br.readLine();
+            System.out.println("Password : ");
+            String f_password = br.readLine();
+            System.out.println("Confirm Password : ");
+            String f_cPassword = br.readLine();
+            reg.setUserName(f_user);
+            reg.setEmailId(f_emailId);
+            reg.setPassword(f_password);
+            register();
+
+            default:
+            continue;
+            }
+        }
+
     }
 }
+
