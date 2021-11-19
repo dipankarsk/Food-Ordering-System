396ec4c89f9e4bbcd57a2a714c5d105fc7018015
Shivam20304 <shivam20304@iiitd.ac.in>
2021-11-19 12:20:19 +0530

    DB Connection

--- src/main/java/com/fooddelivery/Database/dbconnection.java
+++ src/main/java/com/fooddelivery/Database/dbconnection.java
@@ -1,5 +1,37 @@
 package com.fooddelivery.Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class dbconnection {
+
+    public static void dbconnect ()
+    {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:/test.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        dbconnect();
+    }
     
 }
